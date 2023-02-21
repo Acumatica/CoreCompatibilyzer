c1c93c61bc21c8174285910c8c0ffe087b867984
Sergey Nikomarov <snikomarov@acumatica.com>
2023-02-21 18:29:17 +0300

    work on VSIX configuration

--- src/CoreCompatibilyzer/CoreCompatibilyzer.Vsix/Properties/AssemblyInfo.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Vsix/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CoreCompatibilyzer.Vsix")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Acumatica")]
 [assembly: AssemblyProduct("CoreCompatibilyzer.Vsix")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © 2017-2023 Acumatica Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
