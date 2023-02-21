77b0151e3a884c71328aa5fd8dc68e694656f827
Sergey Nikomarov <snikomarov@acumatica.com>
2023-02-21 20:37:02 +0300

    Added helper to work with resources

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer/Utils/Resources/ResourcesHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using CoreCompatibilyzer.Utils.Common;
+
+using Microsoft.CodeAnalysis;
+
+namespace CoreCompatibilyzer.Utils.Resources
+{
+    public static class ResourcesHelper
+    {
+        public static LocalizableString GetLocalizedDiagnosticName(this string diagnosticResourceName)
+        {
+            return new LocalizableResourceString(diagnosticResourceName, Diagnostics.ResourceManager, typeof(Diagnostics));
+        }
+
+        public static LocalizableString GetLocalized<TResource>(this string resourceName, ResourceManager resourceManager)
+        {
+            resourceManager.ThrowIfNull(nameof(resourceManager));
+            return new LocalizableResourceString(resourceName, resourceManager, typeof(TResource));
+        }
+    }
+}
