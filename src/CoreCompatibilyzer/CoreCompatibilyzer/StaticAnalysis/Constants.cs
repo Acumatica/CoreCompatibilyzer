39c16ee5131958f982787667fd5699cbe1bcf9e6
Sergey Nikomarov <snikomarov@acumatica.com>
2023-02-22 14:52:55 +0300

    fixed file names

--- src/CoreCompatibilyzer/CoreCompatibilyzer/StaticAnalysis/Constants.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer/StaticAnalysis/Constants.cs
@@ -1,24 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 
-using Microsoft.CodeAnalysis;
-
-using CoreCompatibilyzer.Utils.Common; ;
-
 namespace CoreCompatibilyzer.StaticAnalysis
 {
-	public static class EditorConfig
+	public static class Constants
 	{
-		private const string DotNetDiagnosticOptionsPrefix = "dotnet_diagnostic";
-		private const string Enabled = "enabled";
-
-		public static string GetEnabledFlagFullName(this DiagnosticDescriptor diagnosticDescriptor) =>
-			diagnosticDescriptor.GetDiagnosticOptionFullName(Enabled);
-
-		public static string GetDiagnosticOptionFullName(this DiagnosticDescriptor diagnosticDescriptor, string optionName)
-		{
-			diagnosticDescriptor.ThrowIfNull(nameof(diagnosticDescriptor));
-			return $"{DotNetDiagnosticOptionsPrefix}.{diagnosticDescriptor.Id}.{optionName.ThrowIfNullOrWhiteSpace(nameof(optionName))}";
-		}
+		/// <summary>
+		/// (Immutable) The compatibility analyzer diagnostics prefix.
+		/// </summary>
+		public const string DiagnosticsPrefix = "CoreCompat";
 	}
 }
--- src/CoreCompatibilyzer/CoreCompatibilyzer/StaticAnalysis/EditorConfig.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer/StaticAnalysis/EditorConfig.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Microsoft.CodeAnalysis;
+
+using CoreCompatibilyzer.Utils.Common; ;
+
 namespace CoreCompatibilyzer.StaticAnalysis
 {
-	public static class Constants
+	public static class EditorConfig
 	{
-		/// <summary>
-		/// (Immutable) The compatibility analyzer diagnostics prefix.
-		/// </summary>
-		public const string DiagnosticsPrefix = "CoreCompat";
+		private const string DotNetDiagnosticOptionsPrefix = "dotnet_diagnostic";
+		private const string Enabled = "enabled";
+
+		public static string GetEnabledFlagFullName(this DiagnosticDescriptor diagnosticDescriptor) =>
+			diagnosticDescriptor.GetDiagnosticOptionFullName(Enabled);
+
+		public static string GetDiagnosticOptionFullName(this DiagnosticDescriptor diagnosticDescriptor, string optionName)
+		{
+			diagnosticDescriptor.ThrowIfNull(nameof(diagnosticDescriptor));
+			return $"{DotNetDiagnosticOptionsPrefix}.{diagnosticDescriptor.Id}.{optionName.ThrowIfNullOrWhiteSpace(nameof(optionName))}";
+		}
 	}
 }
