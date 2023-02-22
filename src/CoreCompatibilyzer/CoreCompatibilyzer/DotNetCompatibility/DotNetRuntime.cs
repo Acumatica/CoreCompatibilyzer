cc05da934dbb6f02affdc72ef7e56b7c74fe723f
Sergey Nikomarov <snikomarov@acumatica.com>
2023-02-22 15:03:35 +0300

    added DotNetRunTime version

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer/DotNetCompatibility/DotNetRuntime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CoreCompatibilyzer.DotNetCompatibility
+{
+	public enum DotNetRuntime
+	{
+		DotNetFramework,
+		DotNetCore21,       // For an MVP let's start with 2.1 version
+		DotNetCore22,
+		DotNetCore30,
+		DotNetCore31,
+		DotNet5,
+		DotNet6
+	}
+}
