1cf1bb90c52d9a3f76416e92514f2456034ab0b4
Sergey Nikomarov <snikomarov@acumatica.com>
2023-03-09 18:40:25 +0300

    added console runner project for .Net Framework

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Runner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCompatibilyzer.Runner.NetFramework
+{
+	internal class Runner
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
