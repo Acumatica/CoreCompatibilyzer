fddc02045a87da2d6355c7f3d8aac66e7f6f8cb8
Sergey Nikomarov <snikomarov@acumatica.com>
2023-03-10 19:09:10 +0300

    added company info and copyright

--- src/CoreCompatibilyzer/CoreCompatibilyzer.Vsix/Properties/AssemblyInfo.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Vsix/Properties/AssemblyInfo.cs
@@ -6,13 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CoreCompatibilyzer.Vsix")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Acumatica")]
 [assembly: AssemblyProduct("CoreCompatibilyzer.Vsix")]
-[assembly: AssemblyCopyright("Copyright © 2017-2023 Acumatica Ltd.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © 2023 Acumatica Ltd.")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -29,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
