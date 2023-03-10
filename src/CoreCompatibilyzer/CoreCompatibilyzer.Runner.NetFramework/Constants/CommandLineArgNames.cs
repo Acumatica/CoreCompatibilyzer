3dc67e98610876789a08ada6ce9557dc01f149d0
Sergey Nikomarov <snikomarov@acumatica.com>
2023-03-10 18:19:40 +0300

    Added Constants to the runner

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Constants/CommandLineArgNames.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreCompatibilyzer.Runner.Constants
+{
+	internal static class CommandLineArgNames
+	{
+		public const string ApplyAcumaticaRules = "applyAcumaticaRules";
+		public const string SkipInternalApi = "skipInternalApi";
+		public const string SkipPXHidden = "skipPXHidden";
+		public const string ReportDocGenerationSuppression = "reportDocGenerationSuppression";
+		public const string CodeSources = "codeSources";
+
+		public const string MSBuildPath = "msBuildPath";
+	}
+}
---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreCompatibilyzer.Runner.Constants
+{
+	internal static class Constants
+	{
+		public const string ProjectFileExtension = ".csproj";
+		public const string SolutionFileExtension = ".sln";
+	}
+}
