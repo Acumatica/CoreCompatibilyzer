bfb7c85611281f297f85c3df7c03bba2398d16c7
Sergey Nikomarov <snikomarov@acumatica.com>
2023-03-10 19:25:11 +0300

    Added basic command line args

--- src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Constants/CommandLineArgNames.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Constants/CommandLineArgNames.cs
@@ -6,12 +6,7 @@ namespace CoreCompatibilyzer.Runner.Constants
 {
 	internal static class CommandLineArgNames
 	{
-		public const string ApplyAcumaticaRules = "applyAcumaticaRules";
-		public const string SkipInternalApi = "skipInternalApi";
-		public const string SkipPXHidden = "skipPXHidden";
-		public const string ReportDocGenerationSuppression = "reportDocGenerationSuppression";
-		public const string CodeSources = "codeSources";
-
+		public const string CodeSource = "codeSource";
 		public const string MSBuildPath = "msBuildPath";
 	}
 }
---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Input/CommandLineOptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using CommandLine;
+
+using CoreCompatibilyzer.Runner.Constants;
+
+namespace CoreCompatibilyzer.Runner.Input
+{
+	internal class CommandLineOptions
+	{
+		[Option(longName: CommandLineArgNames.CodeSource, Required = true,
+				HelpText = "A path to the \"code source\" which will be validated. The term \"code source\" is a generalization for components/services that can provide source code to the tool.\n" +
+						   "Currenly the supported code sources are C# projects and C# solutions.")]
+		public string CodeSource { get; }
+
+		/// <summary>
+		/// Optional explicitly specified path to MSBuild. Can be null. If null then MSBuild path is retrieved automatically.
+		/// </summary>
+		/// <value>
+		/// The optional explicitly specified path to MSBuild.
+		/// </value>
+		[Option(longName: CommandLineArgNames.MSBuildPath,
+				HelpText = "This parameter allows you to provide explicitly a path to MSBuild tool that will be used for analysis.")]
+		public string? MSBuildPath { get; }
+
+		// Constructor arguments order must be the same as the properties order. This allows command line parser to initialize immutable options object via constructor.
+		// See this for details: https://github.com/commandlineparser/commandline/wiki/Immutable-Options-Type
+		public CommandLineOptions(string codeSource, string? msBuildPath)
+		{
+			CodeSource = codeSource;
+			MSBuildPath = msBuildPath;
+		}
+	}
+}
