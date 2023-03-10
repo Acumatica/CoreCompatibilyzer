44f670fc257bc83101cdc95c57770c2001910264
Sergey Nikomarov <snikomarov@acumatica.com>
2023-03-10 18:01:04 +0300

    added basic Serilog logging

--- src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Runner.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Runner.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
 using CoreCompatibilyzer.Utils.Runner;
+
+using Serilog;
 
 namespace CoreCompatibilyzer.Runner.NetFramework
 {
@@ -12,9 +15,28 @@ namespace CoreCompatibilyzer.Runner.NetFramework
 	{
 		static int Main(string[] args)
 		{
+			try
+			{
+				var validationResult = RunValidation();
+				return validationResult.ToExitCode();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				return RunResult.RunTimeError.ToExitCode();
+			}
+		}
 
+		private static void ConfigureLogger()
+		{
+			var loggerConfiguration = new LoggerConfiguration().WriteTo.Console()
+															   .Enrich.FromLogContext();
+			Log.Logger = loggerConfiguration.CreateLogger();
+		}
 
-			return RunResult.RunTimeError.ToExitCode();
+		private static RunResult RunValidation()
+		{
+
 		}
 	}
 }
