ab7cf8cf197225a4875c8789c6b8256881bd48a1
Sergey Nikomarov <snikomarov@acumatica.com>
2023-03-12 21:25:52 +0300

    implemented initial Runner startup

--- src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Runner.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Runner.cs
@@ -1,42 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-using CoreCompatibilyzer.Utils.Runner;
+using CommandLine;
+
+using CoreCompatibilyzer.Runner.Analysis;
+using CoreCompatibilyzer.Runner.Input;
 
 using Serilog;
 
 namespace CoreCompatibilyzer.Runner.NetFramework
 {
-	internal class Runner
+    internal class Runner
 	{
-		static int Main(string[] args)
+		static async Task<int> Main(string[] args)
 		{
 			try
 			{
-				var validationResult = RunValidation();
-				return validationResult.ToExitCode();
+				ParserResult<CommandLineOptions> argsParsingResult = Parser.Default.ParseArguments<CommandLineOptions>(args);
+				RunResult runResult = await argsParsingResult.MapResult(parsedFunc: RunValidationWithParsedOptionsAsync,
+																		notParsedFunc: OnParsingErrorsAsync);
+				return runResult.ToExitCode();
 			}
 			catch (Exception e)
 			{
-				Console.WriteLine(e);
+				Log.Error(e, "An unhandled runtime error was encountered during the validation");
 				return RunResult.RunTimeError.ToExitCode();
 			}
 		}
 
-		private static void ConfigureLogger()
+		private static async Task<RunResult> RunValidationWithParsedOptionsAsync(CommandLineOptions commandLineOptions)
 		{
-			var loggerConfiguration = new LoggerConfiguration().WriteTo.Console()
-															   .Enrich.FromLogContext();
-			Log.Logger = loggerConfiguration.CreateLogger();
+			if (!TryInitalizeSerilog(commandLineOptions))
+				return RunResult.RunTimeError;
+
+			AnalysisContext? analysisContext = CreateAnalysisContextFromCommandLineArguments(commandLineOptions);
+
+			if (analysisContext == null)
+				return RunResult.RunTimeError;
+
+			var analyzer = new CompatibilityAnalysisRunner();
+			var analysisResult = await analyzer.Analyze(analysisContext, CancellationToken.None);
+			return analysisResult;
 		}
 
-		private static RunResult RunValidation()
+		private static bool TryInitalizeSerilog(CommandLineOptions commandLineOptions)
 		{
+			try
+			{
+				var loggerConfiguration = new LoggerConfiguration().WriteTo.Console()
+																   .Enrich.FromLogContext();
+				Log.Logger = loggerConfiguration.CreateLogger();
+				return true;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);   // Log failed serilog initialization directly to console
+				return false;
+			}
+		}
 
+		private static AnalysisContext? CreateAnalysisContextFromCommandLineArguments(CommandLineOptions commandLineOptions)
+		{
+			try
+			{
+				AnalysisContextBuilder analysisContextBuilder = new AnalysisContextBuilder();
+				AnalysisContext analysisContext = analysisContextBuilder.CreateContext(commandLineOptions);
+				return analysisContext;
+			}
+			catch (Exception e)
+			{
+				Log.Error(e, "An error happened during the processing of input command line arguments and initialization of analysis context");
+				return null;
+			}
+		}
+
+		private static Task<RunResult> OnParsingErrorsAsync(IEnumerable<Error> parsingErrors)
+		{
+			foreach (var error in parsingErrors) 
+			{
+				Log.Error("Parsing error type: {ErrorType}", error.Tag);
+			}
+
+			return Task.FromResult(RunResult.RunTimeError);
 		}
 	}
 }
