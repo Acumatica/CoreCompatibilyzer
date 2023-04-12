2f2d5b33ca5460e58b356ad1fa4a34a2eece8c2f
Sergey Nikomarov <snikomarov@acumatica.com>
2023-04-12 20:06:25 +0300

    added banned api data provider interface

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer/BannedApiData/Base/IBannedApiDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreCompatibilyzer.BannedApiData
+{
+    public interface IBannedApiDataProvider
+	{
+        IEnumerable<BannedApi> GetBannedApiData();
+    }
+}
