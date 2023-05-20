244b81bef5a575e8dda91a70a639a8f00a2cc372
Sergey Nikomarov <snikomarov@acumatica.com>
2023-05-21 01:42:11 +0300

    integrated changes with base provider into file provider

--- src/CoreCompatibilyzer/CoreCompatibilyzer/BannedApiData/Providers/FileDataProvider.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer/BannedApiData/Providers/FileDataProvider.cs
@@ -9,26 +9,20 @@ using CoreCompatibilyzer.Utils.Common;
 
 namespace CoreCompatibilyzer.BannedApiData
 {
-	public class FileDataProvider : IBannedApiDataProvider
+	public class FileDataProvider : BannedApiDataProvider
 	{
 		private readonly string _filePath;
 
-		public bool IsDataAvailable => File.Exists(_filePath);
+		/// <inheritdoc/>
+		public override bool IsDataAvailable => File.Exists(_filePath);
 
 		public FileDataProvider(string filePath)
 		{
 			_filePath = filePath.ThrowIfNullOrWhiteSpace(nameof(filePath));
 		}
 
-		/// <summary>
-		/// Gets the banned API data asynchronously from the provider or <see langword="null"/> if the provider's banned API data is not available. <br/>
-		/// On the latter case the <see cref="IsDataAvailable"/> flag value is <see langword="false"/>.
-		/// </summary>
-		/// <param name="cancellation">A token that allows processing to be cancelled.</param>
-		/// <returns>
-		/// The task with banned API data.
-		/// </returns>
-		public async Task<IEnumerable<BannedApi>?> GetBannedApiDataAsync(CancellationToken cancellation)
+		/// <inheritdoc/>
+		public override async Task<IEnumerable<BannedApi>?> GetBannedApiDataAsync(CancellationToken cancellation)
 		{
 			cancellation.ThrowIfCancellationRequested();
 
@@ -43,31 +37,17 @@ namespace CoreCompatibilyzer.BannedApiData
 														 .ConfigureAwait(false);
 			}
 
+			cancellation.ThrowIfCancellationRequested();
+
 			if (wholeText.IsNullOrWhiteSpace())
 				return Enumerable.Empty<BannedApi>();
 
-			return GetBannedApiDataFromMemoryText(wholeText, cancellation);
+			var bannedApis = ParseTextIntoBannedApis(wholeText, cancellation);
+			return bannedApis;
 		}
 
-		private IEnumerable<BannedApi> GetBannedApiDataFromMemoryText(string text, CancellationToken cancellation) 
-		{
-			cancellation.ThrowIfCancellationRequested();
-
-			int lineNumber = 1;
-			using StringReader wholeTextReader = new StringReader(text);
-
-			while (wholeTextReader.ReadLine() is string rawDocID)
-			{
-				cancellation.ThrowIfCancellationRequested();
-
-				BannedApi bannedApi = ReadLineFromFile(rawDocID, lineNumber);
-				yield return bannedApi;
-
-				lineNumber++;
-			}
-		}
-
-		public IEnumerable<BannedApi>? GetBannedApiData(CancellationToken cancellation) =>
+		/// <inheritdoc/>
+		public override IEnumerable<BannedApi>? GetBannedApiData(CancellationToken cancellation) =>
 			GetBannedApiDataFromFile(cancellation);
 
 		private IEnumerable<BannedApi>? GetBannedApiDataFromFile(CancellationToken cancellation)
@@ -78,37 +58,12 @@ namespace CoreCompatibilyzer.BannedApiData
 				return null;
 	
 			var lines	   = File.ReadLines(_filePath);
-			var bannedApis = ParseLinesIntoBannedApis(lines);
+			var bannedApis = ParseLinesIntoBannedApis(lines, cancellation);
 
 			return bannedApis;
-
-			//-----------------------------------------Local Function------------------------------------
-			IEnumerable<BannedApi> ParseLinesIntoBannedApis(IEnumerable<string> rawLines)
-			{
-				int lineNumber = 1;
-
-				foreach (string rawDocID in rawLines)
-				{
-					cancellation.ThrowIfCancellationRequested();
-
-					BannedApi bannedApi = ReadLineFromFile(rawDocID, lineNumber);
-					yield return bannedApi;
-
-					lineNumber++;
-				}
-			}
 		}
 
-		private BannedApi ReadLineFromFile(string rawDocID, int lineNumber) 
-		{
-			try
-			{
-				return new BannedApi(rawDocID);
-			}
-			catch (Exception exception)
-			{
-				throw new BannedApiReaderException($"An error happened during the reading of the line {lineNumber} from the file \"{_filePath}\"", exception);
-			}
-		}
+		protected override string GetParseErrorMessage(Exception originalException, int lineNumber) =>
+			$"An error happened during the reading of the line {lineNumber} from the file \"{_filePath}\"";
 	}
 }
