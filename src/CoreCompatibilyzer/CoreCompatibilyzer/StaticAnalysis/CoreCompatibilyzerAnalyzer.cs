b1100b65ea0cbf8391348aab72861f65817055cd
Sergey Nikomarov <snikomarov@acumatica.com>
2023-05-22 13:48:56 +0300

    reworked analyzer to check syntax nodes instead of symbols

--- src/CoreCompatibilyzer/CoreCompatibilyzer/StaticAnalysis/CoreCompatibilyzerAnalyzer.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer/StaticAnalysis/CoreCompatibilyzerAnalyzer.cs
@@ -1,46 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 
-using CoreCompatibilyzer.BannedApiData.Model;
 using CoreCompatibilyzer.BannedApiData.Providers;
 using CoreCompatibilyzer.BannedApiData.Storage;
 using CoreCompatibilyzer.StaticAnalysis.BannedApiRetriever;
-using CoreCompatibilyzer.Utils.Common;
-using CoreCompatibilyzer.Utils.Roslyn.Suppression;
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 
 namespace CoreCompatibilyzer.StaticAnalysis
 {
 	[DiagnosticAnalyzer(LanguageNames.CSharp)]
-	public class CoreCompatibilyzerAnalyzer : DiagnosticAnalyzer
+	public partial class CoreCompatibilyzerAnalyzer : DiagnosticAnalyzer
 	{
 		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
 		   ImmutableArray.Create(
 			   Descriptors.CoreCompat1001_ApiNotPresentInDotNetCore,
 			   Descriptors.CoreCompat1002_ApiObsoleteInDotNetCore);
-
-		protected virtual ImmutableArray<SymbolKind> SymbolKindsToAnalyze =>
-			new[]
-			{
-				SymbolKind.Namespace,
-				SymbolKind.NamedType,
-				SymbolKind.Method,
-				SymbolKind.Property,
-				SymbolKind.Event,
-				SymbolKind.Field,
-				SymbolKind.Parameter,
-				SymbolKind.TypeParameter,
-				SymbolKind.Local,
-				SymbolKind.ArrayType,
-				SymbolKind.PointerType
-			}
-			.ToImmutableArray();
 
 		private readonly IBannedApiStorage? _customBannedApi;
 		private readonly IApiBanInfoRetriever? _customBanInfoRetriever;
@@ -58,18 +41,18 @@ namespace CoreCompatibilyzer.StaticAnalysis
 		public sealed override void Initialize(AnalysisContext context)
 		{
 			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze); // We want to analyze for compatibility even generated code
-			context.EnableConcurrentExecution();
+
+			if (!Debugger.IsAttached)
+			{
+				context.EnableConcurrentExecution();
+			}
+				
 			context.RegisterCompilationStartAction(AnalyzeCompilation);
 		}
 
 		private void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext)
 		{
 			compilationStartContext.CancellationToken.ThrowIfCancellationRequested();
-
-			var symbolKindsToAnalyze = SymbolKindsToAnalyze;
-
-			if (symbolKindsToAnalyze.IsDefaultOrEmpty)
-				return;
 
 			var bannedApiStorage = _customBannedApi ?? GetBannedApiStorage(compilationStartContext.CancellationToken);
 
@@ -83,7 +66,8 @@ namespace CoreCompatibilyzer.StaticAnalysis
 			if (apiBanInfoRetriever == null)
 				return;
 
-			compilationStartContext.RegisterSymbolAction(context => AnalyzeSymbol(context, bannedApiStorage, apiBanInfoRetriever), symbolKindsToAnalyze);
+			compilationStartContext.RegisterSyntaxNodeAction(context => AnalyzeSyntaxTree(context, bannedApiStorage, apiBanInfoRetriever), 
+															 SyntaxKind.CompilationUnit);
 		}
 
 		protected virtual IBannedApiStorage GetBannedApiStorage(CancellationToken cancellation, IBannedApiDataProvider? customBannedApiDataProvider = null) =>
@@ -92,34 +76,15 @@ namespace CoreCompatibilyzer.StaticAnalysis
 		protected virtual IApiBanInfoRetriever GetApiBanInfoRetriever(IBannedApiStorage bannedApiStorage) =>
 			new HierarchicalApiBanInfoRetriever(bannedApiStorage);
 
-		private void AnalyzeSymbol(in SymbolAnalysisContext symbolAnalysisContext, IBannedApiStorage bannedApiStorage, IApiBanInfoRetriever apiBanInfoRetriever)
+		private void AnalyzeSyntaxTree(in SyntaxNodeAnalysisContext syntaxContext, IBannedApiStorage bannedApiStorage, IApiBanInfoRetriever apiBanInfoRetriever)
 		{
-			symbolAnalysisContext.CancellationToken.ThrowIfCancellationRequested();
+			syntaxContext.CancellationToken.ThrowIfCancellationRequested();
 
-			if (apiBanInfoRetriever.GetBanInfoForApi(symbolAnalysisContext.Symbol) is not BannedApi banApiInfo) 
-				return;
-
-			var diagnosticDescriptor = GetDiagnosticFromBannedApiInfo(banApiInfo);
-
-			if (diagnosticDescriptor == null)
-				return;
-
-			symbolAnalysisContext.CancellationToken.ThrowIfCancellationRequested();
-
-			var locations = symbolAnalysisContext.Symbol.Locations;
-
-			foreach (var location in locations) 
+			if (syntaxContext.Node is CompilationUnitSyntax compilationUnitSyntax)
 			{
-				symbolAnalysisContext.ReportDiagnosticWithSuppressionCheck(
-					Diagnostic.Create(diagnosticDescriptor, location, banApiInfo.FullName));
-			}
+				var apiNodesWalker = new ApiNodesWalker(syntaxContext, bannedApiStorage, apiBanInfoRetriever);
+				compilationUnitSyntax.Accept(apiNodesWalker);
+			}	
 		}
-
-		private DiagnosticDescriptor? GetDiagnosticFromBannedApiInfo(in BannedApi banApiInfo) => banApiInfo.BannedApiType switch
-		{
-			BannedApiType.NotPresentInNetCore => Descriptors.CoreCompat1001_ApiNotPresentInDotNetCore,
-			BannedApiType.Obsolete			  => Descriptors.CoreCompat1002_ApiObsoleteInDotNetCore,
-			_								  => null
-		};
 	}
 }
