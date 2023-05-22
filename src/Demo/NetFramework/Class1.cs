31a2eb9f929aa13d78aa87f4cd8f9f4205e1caaa
Sergey Nikomarov <snikomarov@acumatica.com>
2023-05-22 11:26:53 +0300

    Added small demo

--- src/Demo/NetFramework/Class1.cs
+++ src/Demo/NetFramework/Class1.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Web;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DemoNetFramework
 {
     public class Class1
     {
+        public static void Abort() => Thread.CurrentThread.Abort();
+
+
+        public static HttpContext GetHttpContext()
+        {
+            return HttpContext.Current;
+
+		}
     }
 }
