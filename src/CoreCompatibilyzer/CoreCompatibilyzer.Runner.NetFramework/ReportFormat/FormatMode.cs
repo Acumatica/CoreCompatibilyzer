17890a1c2a2e4bccd4968ff9b687a1a0bca84334
Sergey Nikomarov <snikomarov@acumatica.com>
2023-07-17 00:12:25 +0300

    added enums for report options

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/ReportFormat/FormatMode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CoreCompatibilyzer.Runner.ReportFormat
+{
+	/// <summary>
+	/// Report format modes.
+	/// </summary>
+	internal enum FormatMode
+	{
+		/// <summary>
+		/// Format Mode to output only a shortened list of used APIs.
+		/// </summary>
+		UsedAPIsOnly,
+
+		/// <summary>
+		/// Format Mode to output only a detailed list of used APIs with usages locations.
+		/// </summary>
+		UsedAPIsWithUsages
+	}
+}
---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/ReportFormat/GroupingMode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CoreCompatibilyzer.Runner.ReportFormat
+{
+	/// <summary>
+	/// Report grouping modes.
+	/// </summary>
+	[Flags]
+	internal enum GroupingMode
+	{
+		/// <summary>
+		/// No grouping is specified for the report.
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// Group API by namespaces.
+		/// </summary>
+		Namespaces = 0xb001,
+
+		/// <summary>
+		/// Group API by types.
+		/// </summary>
+		Types = 0xb010
+	}
+}
