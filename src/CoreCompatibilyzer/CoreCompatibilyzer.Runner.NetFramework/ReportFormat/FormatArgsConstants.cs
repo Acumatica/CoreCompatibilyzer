37b14302485e6d053f6b08d577685bc9db018f86
Sergey Nikomarov <snikomarov@acumatica.com>
2023-07-17 00:36:39 +0300

    added constants for format args

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/ReportFormat/FormatArgsConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreCompatibilyzer.Runner.Constants
+{
+	internal static class FormatArgsConstants
+	{
+		public const string UsedAPIsOnly	   = "apis-only";
+		public const string UsedAPIsWithUsages = "apis-with-usages";
+	}
+}
