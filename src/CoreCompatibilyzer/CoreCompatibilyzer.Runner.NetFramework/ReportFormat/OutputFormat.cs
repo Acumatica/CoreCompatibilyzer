e2c25aec1af0ff4c3abbe09a977c111833d7b5e0
Sergey Nikomarov <snikomarov@acumatica.com>
2023-07-28 21:32:38 +0300

    added output format enum

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/ReportFormat/OutputFormat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CoreCompatibilyzer.Runner.Output
+{
+	/// <summary>
+	/// Report output data format.
+	/// </summary>
+	internal enum OutputFormat
+	{
+		/// <summary>
+		/// Output report results as a plain text
+		/// </summary>
+		PlainText,
+
+		/// <summary>
+		/// Output report results as a json.
+		/// </summary>
+		Json
+	}
+}
