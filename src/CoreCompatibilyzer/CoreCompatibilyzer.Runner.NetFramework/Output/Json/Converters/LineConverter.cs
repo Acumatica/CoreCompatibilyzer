fb5757f1b3691222716c2c8f129eb0e9bbe959e1
Sergey Nikomarov <snikomarov@acumatica.com>
2023-08-12 15:30:59 +0300

    added JSON converters

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Output/Json/Converters/LineConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+
+using CoreCompatibilyzer.Runner.Output.Data;
+
+using Serilog;
+
+namespace CoreCompatibilyzer.Runner.Output.Json
+{
+	/// <summary>
+	/// JSON line converter.
+	/// </summary>
+	internal class LineConverter : JsonConverter<Line>
+	{
+		public override Line Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+			throw new NotSupportedException();
+
+		public override void Write(Utf8JsonWriter writer, Line line, JsonSerializerOptions options)
+		{
+			switch (line.Spans.Length)
+			{
+				case 0:
+					return;
+
+				case 2:
+					var (fullApiName, location) = (line.Spans[0].ToString(), line.Spans[1].ToString());
+
+					writer.WriteStringValue($"{fullApiName}: {location}");
+					return;
+
+				default:
+					string lineStr = line.ToString();
+
+					if (!string.IsNullOrWhiteSpace(lineStr))
+						writer.WriteStringValue(lineStr);
+
+					return;
+			}
+		}
+	}
+}
---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Output/Json/Converters/TitleConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+
+using CoreCompatibilyzer.Runner.Output.Data;
+
+using Serilog;
+
+namespace CoreCompatibilyzer.Runner.Output.Json
+{
+	/// <summary>
+	/// JSON title converter.
+	/// </summary>
+	internal class TitleConverter : JsonConverter<Title>
+	{
+		public override Title Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+			throw new NotSupportedException();
+
+		public override void Write(Utf8JsonWriter writer, Title title, JsonSerializerOptions options)
+		{
+			if (!string.IsNullOrWhiteSpace(title.Text))
+				writer.WriteStringValue(title.Text);
+		}
+	}
+}
