3ee0598a403ac15c632a9640418eb75050a513de
Sergey Nikomarov <snikomarov@acumatica.com>
2023-08-30 14:20:30 +0300

    moved calculation of distinct APIs based on the app settings into a separate class

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Output/UsedDistinctApisCalculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using CoreCompatibilyzer.ApiData.Model;
+using CoreCompatibilyzer.Runner.Input;
+using CoreCompatibilyzer.Utils.Common;
+using CoreCompatibilyzer.Runner.Output.Data;
+
+using Microsoft.CodeAnalysis;
+
+namespace CoreCompatibilyzer.Runner.Output
+{
+	internal class UsedDistinctApisCalculator
+	{
+		private readonly HashSet<string> _usedNamespaces;
+		private readonly HashSet<string> _usedBannedTypes;
+
+		private readonly bool _effectiveShowMemberOfUsedType;
+
+		public UsedDistinctApisCalculator(AppAnalysisContext analysisContext, HashSet<string> usedNamespaces, HashSet<string> usedBannedTypes)
+		{
+			analysisContext.ThrowIfNull(nameof(analysisContext));
+
+			_usedNamespaces  = usedNamespaces ?? new HashSet<string>();
+			_usedBannedTypes = usedBannedTypes ?? new HashSet<string>();
+
+			_effectiveShowMemberOfUsedType = analysisContext.ReportMode == ReportMode.UsedAPIsWithUsages || // Always display members of used types if the report is configured to display usages
+											 analysisContext.ShowMembersOfUsedType;
+		}
+
+		/// <summary>
+		/// Gets all unsorted used apis in the <paramref name="unsortedDiagnostics"/>.
+		/// </summary>
+		/// <param name="unsortedDiagnostics">The unsorted diagnostics.</param>
+		/// <returns/>
+		public IEnumerable<Api> GetAllUsedApis(IEnumerable<(Diagnostic Diagnostic, Api BannedApi)> unsortedDiagnostics) =>
+			GetAllUsedApisImpl(unsortedDiagnostics.ThrowIfNull(nameof(unsortedDiagnostics)));
+
+		private IEnumerable<Api> GetAllUsedApisImpl(IEnumerable<(Diagnostic Diagnostic, Api BannedApi)> unsortedDiagnostics)
+		{
+			var distinctApis = unsortedDiagnostics.Select(d => d.BannedApi)
+												  .Distinct();
+			foreach (Api api in distinctApis)
+			{
+				switch (api.Kind)
+				{
+					case ApiKind.Namespace:
+						if (_usedNamespaces.Contains(api.Namespace))
+							yield return api;
+
+						continue;
+
+					case ApiKind.Type
+					when _effectiveShowMemberOfUsedType || api.AllContainingTypes.IsDefaultOrEmpty || !AreContainingTypesUsed(api):
+						yield return api;
+						continue;
+
+					case ApiKind.Field:
+					case ApiKind.Property:
+					case ApiKind.Event:
+					case ApiKind.Method:
+						if (_effectiveShowMemberOfUsedType || !_usedBannedTypes.Contains(api.FullTypeName))
+							yield return api;
+
+						continue;
+				}
+			}
+		}
+
+		private bool AreContainingTypesUsed(Api api)
+		{
+			string containingTypeName = $"{api.Namespace}";
+
+			for (int i = 0; i < api.AllContainingTypes.Length; i++)
+			{
+				containingTypeName += $".{api.AllContainingTypes[i]}";
+
+				if (_usedBannedTypes!.Contains(containingTypeName))
+					return true;
+			}
+
+			return false;
+		}
+	}
+}
