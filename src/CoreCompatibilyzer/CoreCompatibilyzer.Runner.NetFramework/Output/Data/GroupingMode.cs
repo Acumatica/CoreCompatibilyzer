d8c8df96c47e98b79494cad0966b671690d72a48
Sergey Nikomarov <snikomarov@acumatica.com>
2023-08-30 14:05:23 +0300

    moved model files to Data folder

---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Output/Data/GroupingMode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CoreCompatibilyzer.Runner.Output.Data
+{
+    /// <summary>
+    /// Report grouping modes.
+    /// </summary>
+    [Flags]
+    internal enum GroupingMode
+    {
+        /// <summary>
+        /// No grouping is specified for the report.
+        /// </summary>
+        None = 0b0000,
+
+        /// <summary>
+        /// Group API calls by namespaces.
+        /// </summary>
+        Namespaces = 0xb0001,
+
+        /// <summary>
+        /// Group API calls by types.
+        /// </summary>
+        Types = 0xb0010,
+
+        /// <summary>
+        /// Group API calls by API.
+        /// </summary>
+        Apis = 0xb0100
+    }
+
+    internal static class GroupingModeExtensions
+    {
+        public static bool HasGrouping(this GroupingMode groupingMode, GroupingMode groupingToCheck) =>
+            (groupingMode & groupingToCheck) == groupingToCheck;
+    }
+}
---
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Output/Data/ReportMode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CoreCompatibilyzer.Runner.Output.Data
+{
+    /// <summary>
+    /// Report format modes.
+    /// </summary>
+    internal enum ReportMode
+    {
+        /// <summary>
+        /// Report mode to output only a shortened list of used banned APIs.
+        /// </summary>
+        UsedAPIsOnly,
+
+        /// <summary>
+        /// Report mode to output only a detailed list of used banned APIs with usages locations.
+        /// </summary>
+        UsedAPIsWithUsages
+    }
+}
