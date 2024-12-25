3210e7e2566b7a05fd87512faf9109a0538f66c5
Sergey Nikomarov <snikomarov@acumatica.com>
2024-12-25 01:37:28 +0100

    AC-333341: reworked grouping modes, eliminated two non-intersecting subsets, fixed bug in enum values

--- src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Output/Data/GroupingMode.cs
+++ src/CoreCompatibilyzer/CoreCompatibilyzer.Runner.NetFramework/Output/Data/GroupingMode.cs
@@ -2,45 +2,43 @@
 
 namespace CoreCompatibilyzer.Runner.Output.Data
 {
-    /// <summary>
-    /// Report grouping modes.
-    /// </summary>
-    [Flags]
-    internal enum GroupingMode
-    {
-        /// <summary>
-        /// No grouping is specified for the report.
-        /// </summary>
-        None = 0b0000,
+	/// <summary>
+	/// Report grouping modes.
+	/// </summary>
+	[Flags]
+	internal enum GroupingMode
+	{
+		/// <summary>
+		/// No grouping is specified for the report.
+		/// </summary>
+		None = 0b0000,
 
-        /// <summary>
-        /// Group API calls by namespaces.
-        /// </summary>
-        Namespaces = ByUsedApi | 0xb0001,
+		/// <summary>
+		/// Group API calls by namespaces.
+		/// </summary>
+		Namespaces = 0b0001,
 
-        /// <summary>
-        /// Group API calls by types.
-        /// </summary>
-        Types = ByUsedApi | 0xb0010,
+		/// <summary>
+		/// Group API calls by types.
+		/// </summary>
+		Types = 0b0010,
 
-        /// <summary>
-        /// Group API calls by API.
-        /// </summary>
-        Apis = ByUsedApi | 0xb0100,
+		/// <summary>
+		/// Group API calls by API.
+		/// </summary>
+		Apis = 0b0100,
 
-        ByUsedApi = 0b1000,
+		/// <summary>
+		/// Group API calls by source file.
+		/// </summary>
+		Files = 0b1000
 
-        // todo: xml
+		//  TODO: add other grouping modes, not only by files
+	}
 
-        Files = BySource | 0b0001_0000,
-
-        BySource = 0b1000_0000,
-
-    }
-
-    internal static class GroupingModeExtensions
-    {
-        public static bool HasGrouping(this GroupingMode groupingMode, GroupingMode groupingToCheck) =>
-            (groupingMode & groupingToCheck) == groupingToCheck;
-    }
+	internal static class GroupingModeExtensions
+	{
+		public static bool HasGrouping(this GroupingMode groupingMode, GroupingMode groupingToCheck) =>
+			(groupingMode & groupingToCheck) == groupingToCheck;
+	}
 }
